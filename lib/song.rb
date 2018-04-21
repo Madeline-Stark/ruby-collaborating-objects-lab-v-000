8d55316bc36f810b745831b206819f4586196f6b
Madeline Stark <madeline.stark1@gmail.com>
2018-04-21 22:46:13 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,22 +17,11 @@ class Song
 
 
   def self.new_by_filename(filename)
-    #binding.pry
     song = Song.new(filename.split(" - ")[1])
     song.name = filename.split(" - ")[1]
     song.artist = filename.split(" - ")[0]
     song.artist_name = filename.split(" - ")[0]
     song
-    #song = filename.split(" - ")
-    #song[0] = song[0].split("/")
-
-    #song[0] = song[0][4]
-    #song.artist_name = song[1]
-    #song.join[" - "]
-    #Song .new_by_filename creates a new instance of a song from the file that's passed in
-    #Failure/Error: new_instance = Song.new_by_filename(file_name)
-    #NoMethodError:
-      #undefined method `new_by_filename' for Song:Class
   end
 
   def add_song(song)
