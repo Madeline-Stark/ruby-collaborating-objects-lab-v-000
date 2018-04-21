54f16d6fc3257e71de8e9bfb403027d5d11fb55b
Madeline Stark <madeline.stark1@gmail.com>
2018-04-21 22:41:01 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -6,28 +6,23 @@ class MP3Importer
     @path = path
   end
 
-  def normalize(file)
 
-  end
 
   def files
     #need to normailze filename with own method that you call within this one
     directory = Dir.glob("#{path}/*mp3")
     new_dir = []
     directory.each do |file|
+      binding.pry
       song = self.normalize(file)
       #binding.pry
       new_dir << song
     end
     new_dir
-    #MP3Importer #files loads all the mp3 files in the path directory
-     #Failure/Error: expect(music_importer.files.size).to eq(4)
-     #NoMethodError:
-       #    undefined method `new_by_filename' for Artist:Class
-      #6) MP3Importer #files normalizes the filename to just the mp3 filename with no path
-      #  Failure/Error: expect(music_importer.files).to include("Action Bronson - Larry Csonka - indie.mp3")
-      #  NoMethodError:
-      #    undefined method `new_by_filename' for #<MP3Importer:0x000000026c6b80 @path="./spec/fixtures/mp3s">
+  end
+
+  def normalize(file)
+    #need to normailze file
   end
 
   def import
